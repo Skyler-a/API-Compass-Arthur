b9c8085ef2447760f6c13283080c50c54ce484d7
Skyler-a <77992926+Skyler-a@users.noreply.github.com>
2022-05-02 15:42:10 -0300

    Adição de funções e bugfix
    Comentário também incluídos

--- controllers/task.js
+++ controllers/task.js
@@ -1,12 +1,39 @@
 const Tasks = require('../models/task')
 
 module.exports = app => {
-
+    // Puxar todas as Tasks existentes do banco de dados
     app.get('/api/v1/task', (req, res) =>  {
         Tasks.mostrarTodasAsTaskCadastradas(res);
     })
+
+    // Puxar uma task do banco de dados pelo ID dela
+    app.get('/api/v1/task/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Tasks.mostrarTaskPorID(id, res)
+    })
+
+    // Puxar todas as tasks relacionadas ao ID de usuário
+    app.get('/api/v1/user/task/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Tasks.mostrarTaskRelacionadaAoUsuario(id, res)
+    })
+
+    // Cadastrar uma nova task
     app.post('/api/v1/task', (req, res) => {
         const task = req.body
         Tasks.criarUmaNovaTask(task, res)
     })
+
+    // Deletar uma Task
+    app.delete('/api/v1/task/:id',(req, res) => {
+        const id = parseInt(req.params.id)
+        Tasks.deletarUmaTask(id, res)
+    })
+
+    // Atualizar uma Task
+    app.put('/api/v1/task/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const values = req.body
+        Tasks.updatePorPut(id, values, res)
+    })    
 }
--- controllers/users.js
+++ controllers/users.js
@@ -2,33 +2,38 @@ const Users = require('../models/users')
 
 module.exports = app => {
 
+    // Puxar todos os usuários cadastrados
     app.get('/api/v1/user', (req, res) =>  {
         Users.mostrarTodosOsUsuariosCadastrados(res)
     })
 
-
+    // Puxar um usuário cadastrado pelo ID dele
     app.get('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
         Users.localizarPorId(id, res)
     })
 
+    // Cadastrar um novo usuário
     app.post('/api/v1/user', (req, res) => {
         const users = req.body
         Users.cadastrarNovoUsuario(users, res)
     })
 
+    // Atualizar um usuário, não sendo necessário passar todos os campos
     app.put('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
         const values = req.body
         Users.updatePorPut(id, values, res)
     })
 
+    // Atualizar um usuário sendo necessário passar todos os campos
     app.patch('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
         const values = req.body
         Users.updatePorPatch(id, values, res)
     })
 
+    // Deletar um usuário
     app.delete('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
         Users.delete(id, res)

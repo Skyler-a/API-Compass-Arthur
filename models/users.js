e583c82d649c40f92d6428775df3e5878f7f17ea
Skyler-a <77992926+Skyler-a@users.noreply.github.com>
2022-05-02 17:43:25 -0300

    Adição da verificação se usuário existe

--- models/users.js
+++ models/users.js
@@ -7,7 +7,7 @@ class Users {
         const birthDate = moment(users.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
         const birthDateIsValid = moment().diff(birthDate, 'years', false) >= 18
         const passwordIsValid = users.password.length >= 6
-        const emailIsValid =  /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(users.email) 
+        const emailIsValid = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(users.email)
         //Validações para que o usuário seja cadastrado com sucesso
         const validation = [
             {
@@ -31,15 +31,15 @@ class Users {
         const erros = validation.filter(campo => !campo.valido)
         const existemErros = erros.length
 
-        if(existemErros) {
+        if (existemErros) {
             res.status(400).json(erros)
         } else {
-            const bDate = {...users, birthDate}
+            const bDate = { ...users, birthDate }
 
             const sql = 'INSERT INTO usuarios SET ?'
 
             conexao.query(sql, bDate, (err, results) => {
-                if(err) {
+                if (err) {
                     res.status(400).json(err)
                 } else {
                     res.status(201).json(users)
@@ -48,13 +48,13 @@ class Users {
 
         }
 
-    } 
+    }
     //Função para mostrar todos os usuários cadastrados
     mostrarTodosOsUsuariosCadastrados(res) {
         const sql = 'SELECT * FROM usuarios'
 
         conexao.query(sql, (err, results) => {
-            if(err) {
+            if (err) {
                 res.status(404).json(err)
             } else {
                 res.status(200).json(results)
@@ -67,65 +67,104 @@ class Users {
     localizarPorId(id, res) {
         const sql = `SELECT * FROM usuarios WHERE id=${id}`
 
-            conexao.query(sql, (err, results) => {
+        conexao.query(sql, (err, results) => {
             const user = results[0]
             if (results.length == 0) {
-                res.status(404).json([{message: 'Usuário não encontrado'}])
+                res.status(404).json([{ message: 'Usuário não encontrado' }])
             } else if (err) {
                 res.status(500).json(err)
             } else {
                 res.status(200).json(user)
             }
         })
-            
+
     }
     //Função para atualizar um usuário sem ser necessário passar todos os campos
     updatePorPut(id, values, res) {
-        if(values.birthDate) {
-            values.birthDate = moment(values.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
-        }
+        if (this.localizarPorId) {
+            const sql = `SELECT * FROM usuarios WHERE id=${id}`
 
-        const sql = 'UPDATE usuarios SET ? WHERE id=?'
+            conexao.query(sql, (err, results) => {
+                const user = results[0]
+                if (results.length == 0) {
+                    res.status(404).json([{ message: 'Usuário não encontrado' }])
+                } else if (err) {
+                    res.status(500).json(err)
+                } else {
+                    if (values.birthDate) {
+                        values.birthDate = moment(values.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
+                    }
 
-        conexao.query(sql, [values, id], (err, results) => {
-            if(err) {
-                res.status(404).json(err)
-            } else {
-                res.status(201).json({...values, id}) 
+                    const sql = 'UPDATE usuarios SET ? WHERE id= ? '
+
+                    conexao.query(sql, [values, id], (err, results) => {
+                        if (err) {
+                            res.status(404).json(err)
+                        } else {
+                            res.status(200).json({ ...values, id })
+                        }
+                    })
+                }
             }
-        })
+            )
+        }
     }
     //Função para atualizar um usuário sendo nessessário passar todos os campos
     updatePorPatch(id, values, res) {
-        if(values.birthDate) {
-            values.birthDate = moment(values.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
-        }
+        if (this.localizarPorId) {
+            const sql = `SELECT * FROM usuarios WHERE id=${id}`
 
-        const sql = 'UPDATE usuarios SET ? WHERE id= ? '
+            conexao.query(sql, (err, results) => {
+                const user = results[0]
+                if (results.length == 0) {
+                    res.status(404).json([{ message: 'Usuário não encontrado' }])
+                } else if (err) {
+                    res.status(500).json(err)
+                } else {
+                    if (values.birthDate) {
+                        values.birthDate = moment(values.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
+                    }
 
-        conexao.query(sql, [values, id], (err, results) => {
-            if(err) {
-                res.status(404).json(err)
-            } else {
-                res.status(200).json({...values, id}) 
+                    const sql = 'UPDATE usuarios SET ? WHERE id= ? '
+
+                    conexao.query(sql, [values, id], (err, results) => {
+                        if (err) {
+                            res.status(404).json(err)
+                        } else {
+                            res.status(200).json({ ...values, id })
+                        }
+                    })
+                }
             }
-        })
+            )
+        }
     }
     //Função para deletar um usuário
     delete(id, res) {
-        const sql = 'DELETE FROM usuarios WHERE id=?'
+        if (this.localizarPorId) {
+            const sql = `SELECT * FROM usuarios WHERE id=${id}`
 
-        conexao.query(sql, id, (err, results) => {
-            if(err) {
-                res.status(404).json(err)
-            } else {
-                res.status(200).json(`O usuário de id ${id} foi deletado com sucesso`)
+            conexao.query(sql, (err, results) => {
+                const user = results[0]
+                if (results.length == 0) {
+                    res.status(404).json([{ message: 'Usuário não encontrado' }])
+                } else if (err) {
+                    res.status(500).json(err)
+                } else {
+                    const sql = 'DELETE FROM usuarios WHERE id=?'
+                    conexao.query(sql, id, (err, results) => {
+                        if (err) {
+                            res.status(404).json(err)
+                        } else {
+                            res.status(200).json(`O usuário de id ${id} foi deletado com sucesso`)
 
+                        }
+                    })
+                }
             }
-        })
+            )
+        }
     }
-    
 }
-
 module.exports = new Users
 
